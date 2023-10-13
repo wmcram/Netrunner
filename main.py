f87fc8caeebb33bffeb44f5610e6a13374864eb6
Wil Cram <wcram17@gmail.com>
2023-10-13 00:03:32 -0500

    dungeon generation + movement

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import tcod
+from engine import Engine
+from input_handlers import EventHandler
+from procgen import generate_dungeon
+from entity import Entity
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
     
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
     
     tileset = tcod.tileset.load_tilesheet(
         "assets/tileset.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     
+    event_handler = EventHandler()
+    
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+    
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player,
+    )
+    
+    engine = Engine(entities=entities, event_handler=event_handler, player=player, game_map=game_map)
+    
     with tcod.context.new_terminal(
         screen_width, screen_height, tileset=tileset, title="Pyrogue", vsync=True
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        
+        #context.sdl_window.fullscreen = True
+        
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-            context.present(root_console)
-            
-            for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
     
 if __name__ == "__main__":
     main()
